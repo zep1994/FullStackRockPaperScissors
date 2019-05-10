8f115f7951fd15d98cdfc207198946f5c691269f
zep1994 <snaxbags@protonmail.com>
2019-05-10 10:51:36 -0400

    Setup 2 clients

---
+++ client1/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import RPS from './components/RPS';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <RPS />
+      </header>
+    </div>
+  );
+}
+
+export default App;
--- server/server.js
+++ server/server.js
@@ -1,13 +1,21 @@
 const express = require('express')
 const morgan = require('morgan')
-
-
 const app = express()
 
 app.use(morgan('tiny'))
+
+app.use(cors({
+    origin: 'http://localhost:3000',
+    credentials: true
+}));
+
+app.use(cors({
+    origin: 'http://localhost:3001',
+    credentials: true
+}));
 
 app.get('/', (req, res) => {
     res.json({message: "hello world"})
 })
 
-app.listen(3000)
+app.listen(3001)
